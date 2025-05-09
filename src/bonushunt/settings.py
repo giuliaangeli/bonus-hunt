8af7e6f3520b3204bb59788e2620267a388ff941
Giulia <giuliaangelioliveira@gmail.com>
2025-05-09 17:46:03 -0300

    chore: update to deploy the app in vencel

--- src/bonushunt/settings.py
+++ src/bonushunt/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'django-insecure-p&5(p4(z+bie5p1f%p33!z%cy%jeo6qsu)tah!&0ljs#!@*9tz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
